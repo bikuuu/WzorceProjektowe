b1618fd5b355de242e3fee79dc5cd517c58c4bae
biku <bikuuu@gmail.com>
2020-06-04 21:05:05 +0200

    LekarzFluentBuilder jako wew. klasa static, pole imie i nazwisko jako pozycje wymagane wymagane.

--- src/main/java/fluentBuilder/Application.java
+++ src/main/java/fluentBuilder/Application.java
@@ -3,9 +3,7 @@ package fluentBuilder;
 public class Application {
 
     public static void main(String[] args) {
-        Lekarz lekarz = new LekarzFluentBuilder()
-                .imie("Szymon")
-                .nazwisko("Nowak")
+        Lekarz lekarz = new Lekarz.LekarzFluentBuilder("Szymon", "Nowak")
                 .pesel("78987876632")
                 .specjalizacja("Hirurg naczyniowy")
                 .wynagrodzenie(200)
--- src/main/java/fluentBuilder/Lekarz.java
+++ src/main/java/fluentBuilder/Lekarz.java
@@ -14,47 +14,79 @@ public class Lekarz {
         return imie;
     }
 
-    public void setImie(String imie) {
-        this.imie = imie;
-    }
-
     public String getNazwisko() {
         return nazwisko;
-    }
-
-    public void setNazwisko(String nazwisko) {
-        this.nazwisko = nazwisko;
     }
 
     public String getPesel() {
         return pesel;
     }
 
-    public void setPesel(String pesel) {
-        this.pesel = pesel;
-    }
-
     public String getSpecjalizacja() {
         return specjalizacja;
-    }
-
-    public void setSpecjalizacja(String specjalizacja) {
-        this.specjalizacja = specjalizacja;
     }
 
     public Zmiana getZmiana() {
         return zmiana;
     }
 
-    public void setZmiana(Zmiana zmiana) {
-        this.zmiana = zmiana;
-    }
-
     public int getWynagrodzenie() {
         return wynagrodzenie;
     }
 
-    public void setWynagrodzenie(int wynagrodzenie) {
-        this.wynagrodzenie = wynagrodzenie;
+    public static final class LekarzFluentBuilder {
+
+        private String imie;
+        private String nazwisko;
+        private String pesel;
+        private String specjalizacja;
+        private Zmiana zmiana;
+        private int wynagrodzenie;
+
+        public LekarzFluentBuilder(String imie, String nazwisko) {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+
+        public LekarzFluentBuilder imie(String imie) {
+            this.imie = imie;
+            return this;
+        }
+
+        public LekarzFluentBuilder nazwisko(String nazwisko) {
+            this.nazwisko = nazwisko;
+            return this;
+        }
+
+        public LekarzFluentBuilder pesel(String pesel) {
+            this.pesel = pesel;
+            return this;
+        }
+
+        public LekarzFluentBuilder specjalizacja(String specjalizacja) {
+            this.specjalizacja = specjalizacja;
+            return this;
+        }
+
+        public LekarzFluentBuilder zmiana(Zmiana zmiana) {
+            this.zmiana = zmiana;
+            return this;
+        }
+
+        public LekarzFluentBuilder wynagrodzenie(int wynagrodzenie) {
+            this.wynagrodzenie = wynagrodzenie;
+            return this;
+        }
+
+        public Lekarz lekarzFluentBuild() {
+            Lekarz lekarz = new Lekarz();
+            lekarz.imie = this.imie;
+            lekarz.nazwisko = this.nazwisko;
+            lekarz.pesel = this.pesel;
+            lekarz.specjalizacja = this.specjalizacja;
+            lekarz.zmiana = this.zmiana;
+            lekarz.wynagrodzenie = this.wynagrodzenie;
+            return lekarz;
+        }
     }
 }
